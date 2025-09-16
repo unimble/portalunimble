bbff37b294aaab7c3ac6d543a3e3db8340b85143
unimble team (via WeWeb) <dev@unimble.com.br>
2025-09-16 20:57:38 -0300

    v132 - Incluindo pagina de atas no teste + fix das equipes

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 131;
+const version = 132;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
