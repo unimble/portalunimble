535ca2b4c4b9470aee97640303b8d046b3c982b2
unimble team (via WeWeb) <dev@unimble.com.br>
2025-09-17 15:15:56 -0300

    v133 - deploy

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 132;
+const version = 133;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
