859c39526cd449cfcf14dab6389ea8d3e9287cc4
unimble team (via WeWeb) <dev@unimble.com.br>
2025-10-03 18:24:35 -0300

    v137 - Corrigindo a url unica do item

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 136;
+const version = 137;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
