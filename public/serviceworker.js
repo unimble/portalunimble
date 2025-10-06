c81c26e98879889c83710c4362305812d92c296e
unimble team (via WeWeb) <dev@unimble.com.br>
2025-10-06 20:31:38 -0300

    v142 - Correção da montagem do PE

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 141;
+const version = 142;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
