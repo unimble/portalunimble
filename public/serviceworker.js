412c8dc1b778451211c3c42ea75c867ac42d1ef4
unimble team (via WeWeb) <dev@unimble.com.br>
2025-10-06 20:12:25 -0300

    v141 - Removendo ID do item da url ao excluir item

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 140;
+const version = 141;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
