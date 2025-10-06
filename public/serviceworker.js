e5c5b658cd1e66fb8f8c833708f1b582293ab9f5
unimble team (via WeWeb) <dev@unimble.com.br>
2025-10-06 19:18:52 -0300

    v139 - Corringindo url unico dos itens

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 138;
+const version = 139;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
