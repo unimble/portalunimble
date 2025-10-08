0e15a8a2f3ba7ed85765517eeda4896dfdbdb308
unimble team (via WeWeb) <dev@unimble.com.br>
2025-10-08 17:21:32 -0300

    v145 - Correção de detalhes Kanban + Destque botão de criar minha conta

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 143;
+const version = 145;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
