086b093a732bca763080fe8e949f8308dc029156
unimble team (via WeWeb) <dev@unimble.com.br>
2025-10-09 16:02:44 -0300

    v147 - Testando pag e filter da pagina de equipes

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 145;
+const version = 147;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
