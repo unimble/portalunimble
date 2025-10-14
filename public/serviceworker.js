79e7d7b0ba09cf7ece7b4108df4b6ef165d2180e
unimble team (via WeWeb) <dev@unimble.com.br>
2025-10-14 16:53:39 -0300

    v150 - Corrigindo max height dropdown central

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 149;
+const version = 150;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
