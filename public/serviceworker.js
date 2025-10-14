e092b67afa1e48f42cf5ff1c9a330b86e405c664
unimble team (via WeWeb) <dev@unimble.com.br>
2025-10-14 16:15:55 -0300

    v149 - Atualizando componente de dropdown da Central

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 147;
+const version = 149;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
