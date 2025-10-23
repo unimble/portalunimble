ecfd4a839b0df1352ea2f8f5ddf3aac50f15f505
unimble team (via WeWeb) <dev@unimble.com.br>
2025-10-23 19:41:28 -0300

    v160 - build teste salvar e novo 3

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 159;
+const version = 160;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
